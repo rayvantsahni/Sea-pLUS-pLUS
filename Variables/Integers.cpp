8b18b613df74c4648045b92325a4627956f62e06
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-07 23:05:17 +0530

    Create Integers.cpp

---
+++ Variables/Integers.cpp
@@ -0,0 +1,31 @@
+// Short is normally defined as a 16-bit integer.
+
+short myVariableName1;  //bytes and  stores from -32768 to +32767
+short int myVariableName2;  //stores from -32768 to +32767
+signed short myVariableName3;  // stores from -32768 to +32767
+signed short int myVariableName4;  // stores from -32768 to +32767
+unsigned short myVariableName5;  // stores from 0 to +65535
+unsigned short int myVariableName6;  // stores from 0 to +65535
+
+
+
+
+
+// Int is guaranteed to be 16-bit, but modern implementations use 32-bit for an int.
+
+int myVariableName7;  // stores from -32768 to +32767
+signed int myVariableName8;  // stores from -32768 to +32767
+unsigned int myVariableName9;  // stores from 0 to +65535
+
+
+
+
+
+// Long is a 32-bit number.
+
+long myVariableName10;  // stores from -2147483648 to +2147483647
+long int myVariableName11;  // stores from -2147483648 to +2147483647
+signed long myVariableName12;  // stores from -2147483648 to +2147483647
+signed long int myVariableName13;  // stores from -2147483648 to +2147483647
+unsigned long myVariableName14;  // stores from 0 to +4294967295
+unsigned long int myVariableName15;  // stores from 0 to +4294967295
