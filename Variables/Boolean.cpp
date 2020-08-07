d79be259ee238320e4b7060ad230b02281754e51
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-07 23:10:33 +0530

    Create Boolean.cpp

---
+++ Variables/Boolean.cpp
@@ -0,0 +1,14 @@
+/*
+The bool (boolean) type is a 1-byte data type that is either true or false. A true being any number other than zero and false being zero. The true keyword uses the value 1 to assign true.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  bool canJump = false;
+  bool canDo = true;
+  cout<< "Value of canJump is: "<< canJump<<endl;
+  cout<< "Value of canDo is: "<< canDo<<endl;
+  return 0;
+}
