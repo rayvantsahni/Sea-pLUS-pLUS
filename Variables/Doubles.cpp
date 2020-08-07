24b49f70a71927736cacc10c67eddc0c04d224a3
Rayvant Sahni <38404580+rayvantsahni@users.noreply.github.com>
2020-08-07 23:09:28 +0530

    Create Doubles.cpp

---
+++ Variables/Doubles.cpp
@@ -0,0 +1,13 @@
+/*
+Doubles are like “floats”, which means they can store decimal places. Doubles can generally store more information than a standard float. Their storage size is of 8 bytes.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  double myDouble;   // Created myDouble
+  myDouble = 8.78;   // Stores 8.78 in myDouble
+  cout << "Value of myDouble is: "<< myDouble <<endl;
+  return 0;
+}
